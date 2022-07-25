299ba613ad257b485cb4bb1ced4986ab625f547f
martincavigliasso <martincavigliasso@gmail.com>
2022-07-25 14:57:01 -0300

    desafio opc

---
+++ js/distribuidora.js
@@ -0,0 +1,14 @@
+
+let Nombre = "coder" 
+let Apellido = "house"
+let FechaNacimiento = "21/04/1980"
+let Random 
+const NombreCompleto = "John Smith"
+
+let numeroA = 21 
+
+let numeroB = 3 
+
+let resultado = numeroA / numeroB 
+let empresa = Nombre + " " + Apellido 
+
