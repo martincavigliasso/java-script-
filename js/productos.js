6fe22a5eba699846fc49366382c983df68bf4c9e
martincavigliasso <martincavigliasso@gmail.com>
2022-08-10 11:48:45 -0300

    preentrega

--- js/productos.js
+++ js/productos.js
@@ -1,14 +1,14 @@
 let stockProductos = [
-    {id: 1, nombre: "Buzo 1", tipo: "buzo", desc: "Un buzo que re va con vos", precio: 1200},
-    {id: 2, nombre: "Buzo 2", tipo: "buzo", desc: "Un buzo que re va con vos", precio: 1100},
-    {id: 3, nombre: "Buzo 3", tipo: "buzo", desc: "Un buzo que re va con vos", precio: 1200},
-    {id: 4, nombre: "Buzo 4", tipo: "buzo", desc: "Un buzo que re va con vos", precio: 1400},
-    {id: 5, nombre: "Buzo 5", tipo: "buzo", desc: "Un buzo que re va con vos", precio: 1200},
-    {id: 6, nombre: "Buzo 6", tipo: "buzo", desc: "Un buzo que re va con vos", precio: 1500},
-    {id: 7, nombre: "Remera 1", tipo: "remera", desc: "Una remera que re va con vos", precio: 500},
-    {id: 8, nombre: "Remera 2", tipo: "remera", desc: "Una remera que re va con vos", precio: 500},
-    {id: 9, nombre: "Remera 3", tipo: "remera", desc: "Una remera que re va con vos", precio: 500},
-    {id: 10, nombre: "Remera 4", tipo: "remera", desc: "Una remera que re va con vos", precio: 700},
+    {id: 1, nombre: "aceituna 1", tipo: "aceituna", desc: "aceitunas de la mejor calidad", precio: 1400},
+    {id: 2, nombre: "aceites", tipo: "aceites", desc: "aceites de la mejor calidad", precio: 1000},
+    {id: 3, nombre: "mayonesas", tipo: "mayo", desc: "mayonesas de distintas variedades", precio: 1100},
+    {id: 4, nombre: "salsas", tipo: "salsas", desc: "novedad salsa arcor", precio: 900},
+    {id: 5, nombre: "jugos", tipo: "jugos en sobre", desc: "", precio: 200},
+    {id: 6, nombre: "sales", tipo: "sal", desc: "sal con especies", precio: 500},
+    {id: 7, nombre: "galletitas", tipo: "galletitas", desc: "galletitas con la mejor onda", precio: 200},
+    {id: 8, nombre: "fiambres", tipo: "quesos", desc: "novedad barra paladinni", precio: 2000},
+    {id: 9, nombre: "fiambres", tipo: "jamon", desc: "barra de jamon reducida en sodio paladinni", precio: 2500},
+    {id: 10, nombre: "queso untable", tipo: "untable", desc: "queso untable finlandia ligth", precio: 600},
 ]
 
 let carritoDeCompras = []
@@ -19,16 +19,23 @@ function agregarAlCarrito() {
 
     let elijoProducto = parseInt(prompt('ingrese su ID de su producto')) 
 
-    let productoAgregar = stockProductos.find((el) => el.id == elijoProducto)// busca el producto el primero que cumple es el que trae
+    let productoAgregar = stockProductos.find((el) => el.id == elijoProducto)
     carritoDeCompras.push(productoAgregar)
      console.log(carritoDeCompras)
     actualizarCarrito()
     
 }
-
+agregarAlCarrito()
 function actualizarCarrito() {
     console.log('cantidad de productos agregados ' + carritoDeCompras.length)
-    let suma = carritoDeCompras.reduce((acc, el) => acc + el.precio, 0)// reduce es un metodo que nos permite hacer calculos acc es acumulador q inicia en 0 y el es el elemento del array
+    let suma = carritoDeCompras.reduce((acc, el) => acc + el.precio, 0)
 
     console.log('la suma total de su Carrito es $' + suma)
 }
+actualizarCarrito()
+
+function buscarPorproducto (){
+    console.log ('buscar productos')
+    
+}
+buscarPorproducto()
